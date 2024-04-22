ac03640b01a2dcb55a6bea3e5282189b24166949
minnhhttris <ntmtri02@gmail.com>
2024-04-22 16:25:00 +0700

    them auth header cho nguoi dung va admin

---
+++ src/components/AuthHeader/AuthHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="title">
+      <div class="logo">
+        <img src="../../assets/logo-book.png" alt="Vue" />
+        <span class="titleWeb">NAVYBOOKS</span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@import "AuthHeader.scss";
+</style>
---
+++ src/components/AuthHeaderAdmin/AuthHeaderAdmin.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="navbar bg-body-tertiary">
+      <div class="title">
+        <div class="logo">
+          <router-link to="/admin/home">
+            <img src="../../assets/logo-book.png" alt="Vue" />
+            <span class="titleWeb">NAVYBOOKS</span>
+          </router-link>
+        </div>
+      </div>
+    </nav>
+</template>
+  
+  <style lang="scss" scoped>
+  @import "AuthHeaderAdmin.scss";
+  </style>
