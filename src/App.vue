891783a9c47a67f01ab094a54257137be5cd2b83
minnhhttris <ntmtri02@gmail.com>
2024-04-23 01:59:02 +0700

    layout cac trang

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,47 @@
-<script>
-export default {
-}
+<script setup>
+import "vue3-toastify/dist/index.css";
 </script>
 <template>
-  <h1>Hello, Vue.js!</h1>
+  <div>
+    <component :is="$route.meta.layout">
+      <router-view />
+    </component>
+  </div>
 </template>
-<style>
-.page {
-  max-width: 400px;
-  margin: auto;
+<style lang="scss">
+:root {
+  --primary: #27374D;
+  --primary-alt: #526D82;
+  --grey: #9DB2BF;
+  --dark: #1e293b;
+  --dark-alt: #334155;
+  --light: #DDE6ED;
+  --sidebar-width: 200px;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira sans", sans-serif;
+}
+body {
+  background: var(--light);
+}
+button {
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+.app {
+  display: flex;
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
+    @media (max-width: 1024px) {
+      padding-left: 6rem;
+    }
+  }
 }
 </style>
