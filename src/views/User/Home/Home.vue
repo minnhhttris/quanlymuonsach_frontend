de5d53722994caddb9e764a25e8f944f533a1979
minnhhttris <ntmtri02@gmail.com>
2024-04-24 14:35:15 +0700

    tao cac trang cho user

---
+++ src/views/User/Home/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="containPage">
+    <div>
+      <div class="titlePage d-flex justify-content-center">
+        <h2 class="gradient-text">NAVYBOOKS</h2>
+      </div>
+      <div class="d-flex justify-content-end">
+        <router-link to="/about" class="text-decoration-none">
+          <button class="btnReadMore">Mượn ngay</button>
+        </router-link>
+      </div>
+      <div class="contentPage d-flex flex-column">
+
+        <div class="row">
+          <div class="img-container1 img-container mt-5 ms-2">
+            <img src="../../../assets/imgbanner/book2.avif" alt="Img1">
+          </div>
+          <div class="img-container2 img-container d-flex flex-column justify-content-center align-items-center">
+            <h2>Chào mừng đến với NAVYBOOKS </h2>
+            <text>Nơi mà bạn có thể khám phá ra nhiều chân trời mới qua sách.
+                Những quyển sách hay và sâu sắc đang chờ đón bạn đến để mở rộng vốn tri thức của mình.
+            </text>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="img-container3 img-container mt-5 mb-2 ms-2">
+            <img src="../../../assets/imgbanner/book4.jpeg" alt="Img3">
+          </div>
+          <div class="img-container4 img-container ">
+            <img src="../../../assets/imgbanner/book1.avif" alt="Img4">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+@import "./Home.scss";
+
+.img-container {
+  flex: 1; /* Tự mở rộng để điều chỉnh kích thước */
+  margin-right: 20px; /* Khoảng cách giữa các hình ảnh */
+}
+
+.single {
+  flex-basis: 100%; /* Hình ảnh hàng đầu tiên có chiều rộng 100% */
+}
+
+.row {
+  display: flex; /* Sắp xếp các hình ảnh trong một hàng */
+  margin-bottom: 20px; /* Khoảng cách giữa các hàng */
+}
+
+.row:last-child {
+  margin-bottom: 0; /* Không có khoảng cách dưới hàng cuối cùng */
+}
+</style>
---
+++ src/views/User/Profile/Profile.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="containPage">
+    <div class="titlePage">
+    </div>
+    <div class="contentPage">
+      <section class="h-100 gradient-custom-2">
+        <div class="h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col">
+              <div class="card" style="background-color: #8fabd083">
+                <div class="rounded-top text-white d-flex flex-row bgProfile">
+                  <div class="mt-5 d-flex flex-column justify-content-center">
+                    <div class="image-avatar d-flex justify-content-center" >
+                      <img v-if="data.Avatar != undefined" :src="'http://localhost:3000/' + data.Avatar"
+                        alt="Generic placeholder image" class="img-fluid img-thumbnail mt-4 mb-2"
+                        />
+                    </div>
+                    <div class="d-flex justify-content-center">
+                      <router-link to="/editprofile" style="z-index: 1" class="btn btn-edit">
+                      <button type="button" data-mdb-ripple-color="dark">
+                        Chỉnh sửa
+                      </button>
+                    </router-link>
+                    </div>
+                    
+                  </div>
+                  <div class="" style="margin-top: 130px; margin-left: 20px">
+                    <h5>{{ data.Ten }}</h5>
+                    <p>{{ data.DiaChi }}</p>
+                  </div>
+                </div>
+
+                <div class="card-body text-black">
+                  <div class="mb-5">
+                    <p class="lead fw-normal mb-1 d-flex justify-content-center">Thông tin cá nhân độc giả</p>
+                    <div class="">
+                      <div class="card-body">
+                        <div class="row">
+                          <div class="col-sm-3">
+                            <p class="mb-0">Họ tên</p>
+                          </div>
+                          <div class="col-sm-9">
+                            <p class="text-muted mb-0">{{ data.Ten }}</p>
+                          </div>
+                        </div>
+                        <hr />
+                        <div class="row">
+                          <div class="col-sm-3">
+                            <p class="mb-0">Số điện thoại</p>
+                          </div>
+                          <div class="col-sm-9">
+                            <p class="text-muted mb-0">
+                              {{ data.DienThoai }}
+                            </p>
+                          </div>
+                        </div>
+                        <hr />
+                        <div class="row">
+                          <div class="col-sm-3">
+                            <p class="mb-0">Địa chỉ</p>
+                          </div>
+                          <div class="col-sm-9">
+                            <p class="text-muted mb-0">{{ data.DiaChi }}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+const data = ref({});
+const ID_DG = localStorage.getItem("ID_DG");
+const fetchData = () => {
+  axios
+    .get("http://localhost:3000/authentication/info/" + ID_DG)
+    .then((res) => {
+      data.value = res.data;
+      console.log(data.value);
+    })
+    .catch((err) => console.log(err));
+};
+fetchData();
+</script>
+<style lang="scss" scoped>
+@import "./Profile.scss";
+</style>
